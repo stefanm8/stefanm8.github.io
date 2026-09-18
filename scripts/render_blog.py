f24533edc1494aa4eff814ad9e38e59f7c72061e
stefanm8 <stefanmihartescu@gmail.com>
2026-09-18 20:07:30 +0300

    ci

--- scripts/render_blog.py
+++ scripts/render_blog.py
@@ -1,4 +1,4 @@
-import markdown2, re, glob, pathlib, os
+import markdown2, re, glob, pathlib, os, json
 
 
 def serialize_filename(filename: str, content: str) -> str:
@@ -7,16 +7,33 @@ def serialize_filename(filename: str, content: str) -> str:
     title = re.search(r"<h1>(.*)</h1>", content).group(1)
     return f"{name}_{title.replace(' ', '-')}.html"
 
+def parse_filename(fname: str) -> dict:
+    print(fname)
+    return {
+        "title": fname.split("_")[1].replace(".md", ""),
+        "date": fname.split("_")[0],
+        "path": fname,
+    }
 
 def clean_assets():
     for file in glob.glob("assets/blog/*"):
         if file == ".gitignore":
             continue
         os.remove(file)
-    
+    with open("assets/files.json", "w") as f:
+        f.write(json.dumps([]))
+
+def build_files_json(files):
+    with open("assets/files.json", "w") as f:
+        files_json = []
+        for file in files:
+            file = pathlib.Path(file)
+            files_json.append(parse_filename(file.name))
+        f.write(json.dumps(files_json))
 
 def build_blog():
     files = glob.glob("blog/*.md")
+
     for file in files:
         file = pathlib.Path(file)
         with open(file, "r") as f:
@@ -26,6 +43,8 @@ def build_blog():
         with open(dest, "w") as f:
             f.write(html)
     
+    files = glob.glob("assets/blog/*.html")
+    build_files_json(files)
 
 if __name__ == "__main__":
     clean_assets()
