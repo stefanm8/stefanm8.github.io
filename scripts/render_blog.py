73e417235204cdbadb8652155c4ec009821f27c0
stefanm8 <stefanmihartescu@gmail.com>
2026-09-18 19:38:57 +0300

    scripting

--- scripts/render_blog.py
+++ scripts/render_blog.py
@@ -1,15 +1,32 @@
-import markdown2
-import glob
-import pathlib
+import markdown2, re, glob, pathlib, os
 
-if __name__ == "__main__":
+
+def serialize_filename(filename: str, content: str) -> str:
+    full_name = filename.split(".")
+    name, _ = full_name[0], full_name[1]
+    title = re.search(r"<h1>(.*)</h1>", content).group(1)
+    return f"{name}_{title.replace(' ', '-')}.html"
+
+
+def clean_assets():
+    for file in glob.glob("assets/blog/*"):
+        if file == ".gitignore":
+            continue
+        os.remove(file)
+    
+
+def build_blog():
     files = glob.glob("blog/*.md")
     for file in files:
         file = pathlib.Path(file)
-        dest = pathlib.Path("assets/blog") / file.name.replace(".md", ".html")
         with open(file, "r") as f:
             markdown = f.read()
         html = markdown2.markdown(markdown)
+        dest = pathlib.Path("assets/blog") / serialize_filename(file.name, html)
         with open(dest, "w") as f:
             f.write(html)
     
+
+if __name__ == "__main__":
+    clean_assets()
+    build_blog()
